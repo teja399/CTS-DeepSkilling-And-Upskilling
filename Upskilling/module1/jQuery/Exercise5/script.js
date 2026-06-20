1861395b7989e92899f4643d7937e333c0d34851
teja399 <yallateja08@gmail.com>
2026-06-20 08:32:29 +0530

    Add files via upload

---
+++ Upskilling/module1/jQuery/Exercise5/script.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $("#colorBtn").click(function () {
+
+        $("#colorBox").css("background-color", "red");
+
+    });
+
+    $("#colorBtn").dblclick(function () {
+
+        $("#colorBox").css("background-color", "white");
+
+    });
+
+});
